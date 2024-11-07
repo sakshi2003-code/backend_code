9c41ade59884fa8d336f4a68382e338330ea501e
_sakshi <sakshibijalwan777@gmail.com>
2024-11-07 15:42:38 +0530

    modification in app comments

--- src/app.js
+++ src/app.js
@@ -23,13 +23,16 @@ app.use(express.urlencoded({extended:true,
     limit:"16kb"
 }))
 // folder jha static files wagera store krwani ho
+// Purpose: This middleware serves static files from the "public" folder.
+//  Any files in this folder can be accessed directly by clients.
 app.use(express.static("public"))
 app.use(cookieParser())
 
 
 // router import 
 import userRouter from "./routes/user.routes.js"
-
+// you can choose any name you like for that import. By naming it userRouter, 
+// you’re making it clear that this router specifically handles user-related routes
 
 // routes declartion 
 // routes or controller ek jgah pe ni hn isliye inke liye middleware lana pdega to app.get use ni kr skte direct

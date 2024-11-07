61cc2a303554fdb2d975074ae7567f5a9114a9ff
_sakshi <sakshibijalwan777@gmail.com>
2024-11-07 10:06:49 +0530

    user validation done

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import{Router} from"express";
-import {registerUser} from "../controllers/user.controller.js"
+import {registerUser,loginUser ,logoutUser,refreshAccessToken} from "../controllers/user.controller.js"
 import {upload} from"../middlewares/multer.middleware.js"
+import {verifyJWT} from "../middlewares/auth.middleware.js"
 const router=Router();
 
 router.route("/register").post(
@@ -14,6 +15,20 @@ router.route("/register").post(
             maxCount:1
         }
     ]),
-    registerUser)
+    registerUser
+)
+
+router.route("/login").post(
+    loginUser
+)
+
+// secure routes
+ router.route("/logout").post(verifyJWT,logoutUser)
+//  hmne verifyJWT m next isiliye diya tha taki confuse na ho phle kisko cahlana h yha pe
 // router.route("/login").post(registerUser)
+
+
+// endpoint route for user refresh token
+router.route("/refresh-token").post(refreshAccessToken)
+
 export default router;
